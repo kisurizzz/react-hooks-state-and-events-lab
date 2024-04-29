b7c9c862d04cc70b769f532de3bf1708eefa56ed
Arnold Kisuri <arnoldkisuri7@gmail.com>
2024-04-29 13:20:56 +0300

    Finsh task

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React , {useState} from "react";
 
 function Item({ name, category }) {
+const[isAdded,setIsAdded] = useState(false)
+
+const handleAddToCart = () => {
+  setIsAdded(!isAdded)
+}
+
+  
   return (
-    <li className="">
+    <li className={isAdded ? 'in-cart' : ''}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleAddToCart}>{isAdded ? 'Remove From Cart' : 'Add to Cart'}</button>
     </li>
   );
 }
